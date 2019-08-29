3baa11a00949d950c358d6af5855ce3a4b084aab
Sara Martín <martingutierrezsara@gmail.com>
2019-08-29 20:59:32 +0200

    create click in cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,14 +8,6 @@ const button = document.querySelector('.js-button');
 const optionCard = document.querySelectorAll('.js-radio');
 const cardList = document.querySelector('.js-list');
 
-
-// let cards = [];
-
-//llamar al servidor
-
-
-//seleccionar una opción
-
 function getCardSelected() {
     for (let i = 0; i < optionCard.length; i++) {
         if (optionCard[i].checked) {
@@ -27,14 +19,11 @@ function getCardSelected() {
 function dataFromServer() {
     event.preventDefault();
     const NUMERO = getCardSelected();
-    createList(parseInt(NUMERO));
+
     fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${NUMERO}.json`)
         .then(response => response.json())
         .then(cards => {
-            const listElements = document.querySelectorAll('.hola');
-            for (let i = 0; i < cards.length; i++) {
-                cards[i].addEventListener('click', showCardImage, cards);
-            };
+            createList(cards);
         })
 
 }
@@ -43,31 +32,20 @@ function createList(cards) {
     while (cardList.hasChildNodes()) {
         cardList.removeChild(cardList.firstChild);
     }
-    const urlDefaultImage = `url("https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB")`
-    for (let i = 0; i < cards; i++) {
+    const urlDefaultImage = `url("https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB")`;
+    for (let i = 0; i < cards.length; i++) {
         const createListElement = document.createElement('li');
         createListElement.classList.add('hola');
         createListElement.style.backgroundImage = urlDefaultImage;
 
         cardList.appendChild(createListElement);
+        createListElement.addEventListener('click', function () {
+            if (createListElement.style.backgroundImage === urlDefaultImage) {
+                createListElement.style.backgroundImage = `url('${cards[i].image}')`;
+            }
+        })
     }
 }
-
-function showCardImage(cards) {
-    const listElements = document.querySelectorAll('.hola');
-
-    for (let i = 0; i < listElements.length; i++) {
-        // const createListElement = document.createElement('li');
-        // createListElement.classList.add('hola');
-        listElements.style.backgroundImage = `url('${cards[i].image}')`;
-
-        // cardList.appendChild(createListElement);
-    }
-}
-
-//pintar la lista de cartas
-
-
 
 button.addEventListener('click', dataFromServer);
 
